66b6b6a134b3f73107bd055cd855f80eb56d3c43
Tosh Nandhan <snandhan@chicagobooth.edu>
2019-02-07 04:07:38 +0000

    finished all dynamic gameplay

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,49 @@
+class GameController < ApplicationController
+    def user_plays_rock
+        
+        @computer_move = ["rock", "paper", "scissors"].sample
+        @outcome = ""
+        
+        if @computer_move == "rock"
+            @outcome = "tied"
+        elsif @computer_move == "paper"
+            @outcome = "lost"
+        else @outcome = "won"
+        end
+        
+        render("move_templates/played_rock.html.erb")
+    end
+
+    def user_plays_paper
+        
+        @computer_move = ["rock", "paper", "scissors"].sample
+        @outcome = ""
+        
+        if @computer_move == "rock"
+            @outcome = "won"
+        elsif @computer_move == "paper"
+            @outcome = "tied"
+        else @outcome = "lost"
+        end
+        
+        render("move_templates/played_paper.html.erb")
+    end
+
+    def user_plays_scissors
+        
+        @computer_move = ["rock", "paper", "scissors"].sample
+        @outcome = ""
+        
+        if @computer_move == "rock"
+            @outcome = "lost"
+        elsif @computer_move == "paper"
+            @outcome = "won"
+        else @outcome = "tied"
+        end
+        
+        render("move_templates/played_scissors.html.erb")
+    end    
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  
+  
+  get("/rock", { :controller => "game", :action => "user_plays_rock"})
+  get("/paper", { :controller => "game", :action => "user_plays_paper"})  
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors"})
+  
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # this is the most important file in the application, list of pages that users can find and visit
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
